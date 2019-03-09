89a020027eb6b4b474dc25c16ef4054c3753e91f
Matt Johnson <mjohnson541@gmail.com>
2019-03-09 12:53:12 -0500

    skip PyCall stuff if already installed

--- pyrms/install.jl
+++ pyrms/install.jl
@@ -1,17 +1,24 @@
 using Pkg
 
+pyout = Pipe()
+proc1 = run(pipeline(`python -V`,stdout=pyout))
+close(pyout.in)
+pyversion = chomp(String(read(out)))
+
 #get the python path
-out = Pipe()
-proc = run(pipeline(`which python`,stdout=out))
-close(out.in)
-pypath = chomp(String(read(out)))
+if !("PyCall" in keys(Pkg.installed()))
+    out = Pipe()
+    proc = run(pipeline(`which python`,stdout=out))
+    close(out.in)
+    pypath = chomp(String(read(out)))
 
-#set env variables for installing PyCall
-ENV["CONDA_JL_HOME"] = join(split(pypath,'/')[2:end-2],'/')
-ENV["PYTHON"] = pypath
+    #set env variables for installing PyCall
+    ENV["CONDA_JL_HOME"] = join(split(pypath,'/')[2:end-2],'/')
+    ENV["PYTHON"] = pypath
+    Pkg.add("PyCall")
+    Pkg.build("PyCall")
+end
 
-Pkg.add("PyCall")
-Pkg.build("PyCall")
 Pkg.add("ReactionMechanismSimulator")
 using PyCall
 using ReactionMechanismSimulator

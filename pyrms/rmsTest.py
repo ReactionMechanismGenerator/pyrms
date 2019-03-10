752345dc4f5a051bbff42ad2e47dda0a4e3c5cba
Matt Johnson <mjohnson541@gmail.com>
2019-03-09 20:34:58 -0500

    add test simulating the superminimal example

--- pyrms/rmsTest.py
+++ pyrms/rmsTest.py
@@ -2,7 +2,17 @@ import unittest
 import math
 
 from pyrms import rms
+from diffeqpy import de
 
 class Testrms(unittest.TestCase):
-    def test_import(self):
-        x = rms.ConstantTPDomain
+    def test_simulate(self):
+        phaseDict = rms.readinput("testing/superminimal.rms")
+        spcs = phaseDict["gas"]["Species"]
+        rxns = phaseDict["gas"]["Reactions"]
+        ig = rms.IdealGas(spcs,rxns,name="gas")
+        initialconds = {"T":1000.0,"P":10.0e5,"H2":2.0,"O2":1.0}
+        domain,y0 = rms.ConstantTPDomain(phase=ig,initialconds=initialconds)
+        react = rms.Reactor(domain,y0,(0.0,10.001))
+        sol = de.solve(react.ode,de.CVODE_BDF(),abstol=1e-20,reltol=1e-8)
+        sim = rms.Simulation(sol,domain)
+        self.assertAlmostEqual(rms.molefractions(sim,"H2",3.0),0.34445669,places=3)

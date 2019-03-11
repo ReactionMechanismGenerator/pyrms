bfa801c60a566a41ba4e0478a138c9bf97de3830
Matt Johnson <mjohnson541@gmail.com>
2019-03-11 19:08:53 -0400

    setup PyCall properly whenever install.jl run

--- pyrms/install.jl
+++ pyrms/install.jl
@@ -2,18 +2,18 @@ using Pkg
 
 #get the python path
 if !("PyCall" in keys(Pkg.installed()))
-    out = Pipe()
-    proc = run(pipeline(`which python`,stdout=out))
-    close(out.in)
-    pypath = chomp(String(read(out)))
-
-    #set env variables for installing PyCall
-    ENV["CONDA_JL_HOME"] = join(split(pypath,'/')[2:end-2],'/')
-    ENV["PYTHON"] = pypath
     Pkg.add("PyCall")
-    Pkg.build("PyCall")
-    run(`python2 -m pip install julia`)
 end
+out = Pipe()
+proc = run(pipeline(`which python`,stdout=out))
+close(out.in)
+pypath = chomp(String(read(out)))
+
+#set env variables for installing PyCall
+ENV["CONDA_JL_HOME"] = join(split(pypath,'/')[2:end-2],'/')
+ENV["PYTHON"] = pypath
+
+Pkg.build("PyCall")
 
 Pkg.develop(PackageSpec(url="https://github.com/ReactionMechanismGenerator/ReactionMechanismSimulator.jl"))
 using PyCall

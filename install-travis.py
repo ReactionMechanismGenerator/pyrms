105bfe4be480dc2e96aded8e586424a06b44e9f7
Matt Johnson <mjohnson541@gmail.com>
2019-03-11 19:57:46 -0400

    fix tabbing

--- install-travis.py
+++ install-travis.py
@@ -17,16 +17,16 @@ if not julia_path:
         #install julia
         if os.environ("OSTYPE") == "linux-gnu":
             os.system("""curl -L https://julialang-s3.julialang.org/bin/linux/x64/1.1/julia-1.1.0-linux-x86_64.tar.gz -o "$HOME/Downloads/julia.tar.gz";""")
-    	    os.system("""tar xzf "$HOME/Downloads/julia.tar.gz" -C "$HOME/Downloads";""")
-    	    os.system("""cp -r "$(find "$HOME/Downloads" -maxdepth 2 -name "julia*" -type d | head -n 1)" "{0}";""".format(os.path.join(julia_install_path,'julia')))
+            os.system("""tar xzf "$HOME/Downloads/julia.tar.gz" -C "$HOME/Downloads";""")
+            os.system("""cp -r "$(find "$HOME/Downloads" -maxdepth 2 -name "julia*" -type d | head -n 1)" "{0}";""".format(os.path.join(julia_install_path,'julia')))
         elif "darwin" in os.environ("OSTYPE"):
-    	    os.system("""curl -L https://julialang-s3.julialang.org/bin/mac/x64/1.1/julia-1.1.0-mac64.dmg -o "$HOME/Downloads/julia.dmg";""")
-    	    os.system("""hdiutil attach ~/Downloads/julia.dmg;""")
-    	    os.system("""cp -r /Volumes/Julia*/Julia*/Contents/Resources/julia {0};""".format(os.path.join(julia_install_path,'julia')))
-    	    os.system("""hdiutil detach -force /Volumes/Julia*;""")
+            os.system("""curl -L https://julialang-s3.julialang.org/bin/mac/x64/1.1/julia-1.1.0-mac64.dmg -o "$HOME/Downloads/julia.dmg";""")
+            os.system("""hdiutil attach ~/Downloads/julia.dmg;""")
+            os.system("""cp -r /Volumes/Julia*/Julia*/Contents/Resources/julia {0};""".format(os.path.join(julia_install_path,'julia')))
+            os.system("""hdiutil detach -force /Volumes/Julia*;""")
         else:
-    	    print("could not identify OS type attempting linux style install")
-    	    os.system("""curl -L https://julialang-s3.julialang.org/bin/linux/x64/1.1/julia-1.1.0-linux-x86_64.tar.gz -o "$HOME/Downloads/julia.tar.gz";""")
+            print("could not identify OS type attempting linux style install")
+            os.system("""curl -L https://julialang-s3.julialang.org/bin/linux/x64/1.1/julia-1.1.0-linux-x86_64.tar.gz -o "$HOME/Downloads/julia.tar.gz";""")
             os.system("""tar xzf "$HOME/Downloads/julia.tar.gz" -C "$HOME/Downloads";""")
             os.system("""cp -r "$(find "$HOME/Downloads" -maxdepth 2 -name "julia*" -type d | head -n 1)" "{0}";""".format(os.path.join(julia_install_path,'julia')))
 

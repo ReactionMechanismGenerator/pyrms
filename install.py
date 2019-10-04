5b0748f8b95c933ccc15aa80d4c4145e0a2a9210
Matt Johnson <mjohnson541@gmail.com>
2019-10-04 19:43:55 -0400

    add pyrms/pyrms to python path so rms can be imported

--- install.py
+++ install.py
@@ -32,7 +32,7 @@ if append_pyrms  == 'y':
         bpath = os.path.join(homepath,".bash_profile")
     else:
         bpath = os.path.join(homepath,".bashrc")
-    st = "\n#pyrms\nexport PYTHONPATH=$PYTHONPATH:{0}".format(path)
+    st = "\n#pyrms\nexport PYTHONPATH=$PYTHONPATH:{0}\nexport PYTHONPATH=$PYTHONPATH:{0}/pyrms".format(path)
     with open(bpath, 'a') as bfile:
         bfile.write(st)
 

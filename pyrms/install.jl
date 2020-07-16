bfb9980f49f6648afb7b4cdd4c2862edb6d8e1ef
Matt Johnson <mjohnson541@gmail.com>
2020-07-16 16:55:22 -0400

    since git doesn't work well with conda-forge julia switch to release version

--- pyrms/install.jl
+++ pyrms/install.jl
@@ -1,3 +1,4 @@
 using Pkg
-Pkg.add(PackageSpec(url="https://github.com/ReactionMechanismGenerator/ReactionMechanismSimulator.jl", rev="master"))
+#Pkg.add(PackageSpec(url="https://github.com/ReactionMechanismGenerator/ReactionMechanismSimulator.jl", rev="master"))
+Pkg.add("ReactionMechanismSimulator")
 using ReactionMechanismSimulator

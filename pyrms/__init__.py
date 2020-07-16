9788a8a4c0abfcbc1706779b72f4f0f63e5690f4
Matt Johnson <mjohnson541@gmail.com>
2020-07-15 20:39:16 -0400

    make it possible to install rms from pyrms.install()

--- pyrms/__init__.py
+++ pyrms/__init__.py
@@ -1,1 +1,14 @@
+import os
+import subprocess
 
+script_dir = os.path.dirname(os.path.realpath(__file__))
+
+def install():
+    """
+    Install Julia packages required for diffeqpy.
+    """
+    import julia
+    import diffeqpy
+    julia.install()
+    diffeqpy.install()
+    subprocess.check_call(['julia', os.path.join(script_dir, 'install.jl')])
---
+++ pyrms/install.jl
@@ -0,0 +1,3 @@
+using Pkg
+Pkg.add(PackageSpec(url="https://github.com/ReactionMechanismGenerator/ReactionMechanismSimulator.jl", rev="master"))
+using ReactionMechanismSimulator

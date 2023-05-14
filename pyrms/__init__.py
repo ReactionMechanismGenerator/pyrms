07bc8ba66bdf1ccc9558c536fbc009698156c4ad
Matt Johnson <mjohnson541@gmail.com>
2023-05-14 10:55:20 -0700

    fix init and add install function

--- pyrms/__init__.py
+++ pyrms/__init__.py
@@ -1,1 +1,16 @@
-import rms
+import os
+import subprocess
+
+def install():
+    """
+    Install Julia packages required for diffeqpy.
+    """
+    import julia
+    import diffeqpy
+    julia.install()
+    diffeqpy.install()
+    from julia.api import Julia
+    jl = Julia(compiled_modules=False)
+    from julia import Pkg
+    Pkg.add("ReactionMechanismSimulator")
+    from julia import ReactionMechanismSimulator

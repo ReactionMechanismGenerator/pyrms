080381339ad4a4b9f87db6c9a080054c1e796875
Matt Johnson <mjohnson541@gmail.com>
2024-07-07 19:12:26 -0700

    adapt test

--- pyrms/rmsTest.py
+++ pyrms/rmsTest.py
@@ -1,8 +1,7 @@
 import unittest
-import math
 
 from pyrms import rms
-from diffeqpy import de
+from juliacall import Main as jl
 
 class Testrms(unittest.TestCase):
     def test_simulate(self):
@@ -11,8 +10,8 @@ class Testrms(unittest.TestCase):
         rxns = phaseDict["gas"]["Reactions"]
         ig = rms.IdealGas(spcs,rxns,name="gas")
         initialconds = {"T":1000.0,"P":10.0e5,"H2":2.0,"O2":1.0}
-        domain,y0 = rms.ConstantTPDomain(phase=ig,initialconds=initialconds)
-        react = rms.Reactor(domain,y0,(0.0,10.001))
-        sol = de.solve(react.ode,de.CVODE_BDF(),abstol=1e-20,reltol=1e-8)
+        domain,y0,p = rms.ConstantTPDomain(phase=ig,initialconds=jl.convert(jl.Dict,initialconds))
+        react = rms.Reactor(domain,y0,(0.0,10.001),p=p)
+        sol = rms.solve(react.ode,rms.CVODE_BDF(),abstol=1e-20,reltol=1e-8)
         sim = rms.Simulation(sol,domain)
         self.assertAlmostEqual(rms.molefractions(sim,"H2",3.0),0.34445669,places=3)
